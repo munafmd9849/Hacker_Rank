d70ecdf96173377932a3af1c222d1a312b10a5ff
Munaf <munafmd9849@gmail.com>
2025-03-01 23:38:48 +0530

    Update If-Else_Angle_Classification.c

--- If-Else_Angle_Classification.c
+++ If-Else_Angle_Classification.c
@@ -3,39 +3,49 @@
 #include <math.h>
 #include <stdlib.h>
 
+void output(int);
+
 int main() {
     
-    signed int x;
-    signed int n;
-    scanf("%d",&x);
+    signed int angle;
     
-    if(x==0){
+    scanf("%d",&angle);
+    
+    if(angle==0){
         printf("Acute Angle");
         return 0;
     }
-    
-    if(x>360){
-        n=x/360;
-        x-=n*360;
+    else if(angle>360){
+            angle-=360*(angle/360);
+            output(angle);
     }
-    
-    if(x>0 && x<90){
-        printf("Acute Angle");
-    }
-    else if(x==90){
-        printf("Right Angle");
-    }
-    else if(x>90 && x<180){
-        printf("Obtuse Angle");
-    }
-    else if(x==180){
-        printf("Straight Angle");
-    }
-    else if(x>180 && x<360){
-        printf("Reflex Angle");
-    }
-    else if(x==360 || x==0){
-        printf("Full Rotation");
+    else{
+            output(angle);
     }
     return 0;
 }
+    
+    
+     void output(int x)
+     {
+         if(x>0 && x<90){
+            printf("Acute Angle");
+        }
+        else if(x==90){
+            printf("Right Angle");
+        }
+        else if(x>90 && x<180){
+            printf("Obtuse Angle");
+        }
+        else if(x==180){
+            printf("Straight Angle");
+        }
+        else if(x>180 && x<360){
+            printf("Reflex Angle");
+        }
+        else if(x==360 || x==0){
+            printf("Full Rotation");
+        }
+     }
+    
+    

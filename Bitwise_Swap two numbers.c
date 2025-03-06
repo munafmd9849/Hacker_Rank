9a4abca766d61681e872032e104dcbe754d35965
Munaf <munafmd9849@gmail.com>
2025-03-06 11:51:39 +0530

    Create Bitwise_Swap two numbers.c

---
+++ Bitwise_Swap two numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+    scanf("%d %d",&a,&b);
+    a=a^b;
+    b=a^b;
+    a=a^b;
+    
+    
+    printf("%d\n%d",a,b);
+    
+    
+    return 0;
+}

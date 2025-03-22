d1a2b098ef3b9f64470ab1d71a2ed7c371ac2347
Munaf <munafmd9849@gmail.com>
2025-03-22 18:42:40 +0530

    Create Arrays_Left Rotate an Array by One Position.c

---
+++ Arrays_Left Rotate an Array by One Position.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void leftRotateByOne(int arr[], int n) {
+    int first=arr[0];
+    for(int i=0;i<=n-2;i++){
+        arr[i]=arr[i+1];
+    }
+    arr[n-1]=first;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    leftRotateByOne(arr, n);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}

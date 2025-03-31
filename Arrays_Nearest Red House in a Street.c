b179755a346730210771cc19fe0cb999c7693d58
Munaf <munafmd9849@gmail.com>
2025-03-31 13:29:38 +0530

    Update Arrays_Nearest Red House in a Street.c

--- Arrays_Nearest Red House in a Street.c
+++ Arrays_Nearest Red House in a Street.c
@@ -1,114 +1,53 @@
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
 #include <stdlib.h>
+#include <limits.h>
+
+#define INF 1000000.0
 
 int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    double dist[n];
 
-   int n;
-    scanf("%d",&n);
-    int a[n];
-    for(int i=0;i<=n-1;i++){
-        scanf("%d",&a[i]);
-    }
-    double h[n];
-    for(int i=0;i<n;i++){
-        double stepf=0;
-        if(a[i]==1){
-            h[i]=0;
-            continue;
-        }
-        else if(a[i]==-1){
-            h[i]=-1;
-            continue;
-        }
-        else if(a[i]==2){
-            for(int j=i+1;j<=n-1;j++){
-                if(a[j]==1){
-                    stepf=0.5;
-                    break;
-                }
-                else if(a[j]==-1){
-                    stepf=-1;
-                    break;
-                }
-                else if(a[j]==0 || a[j]==2){
-                    stepf=stepf+0.5;
-                }
-            }
-        }
-        else if(a[i]==0){
-            for(int j=i+1;j<=n-1;j++){
-                if(a[j]==1){
-                    stepf=1;
-                    break;
-                }
-                else if(a[j]==-1){
-                    stepf=-1;
-                    break;
-                }
-                else if(a[j]==0){
-                    stepf=stepf+1;
-                }
-                else if(a[j]==2){
-                    stepf=stepf+0.5;
-                }
-            }
-        }
-        h[i]=stepf;
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        dist[i] = INF;
     }
 
-    double g[n];
-    for(int i=n-1;i>=0;i--){
-        double stepf=0;
-        if(a[i]==1){
-            g[i]=0;
-            continue;
+    // Left to Right pass
+    double nearest = INF;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == 1) {
+            nearest = 0.0;
+        } else if (arr[i] == -1) {
+            nearest = INF;
+        } else if (nearest < INF) {
+            nearest += (arr[i] == 2) ? 0.5 : 1.0;
         }
-        else if(a[i]==-1){
-            g[i]=-1;
-            continue;
-        }
-        else if(a[i]==2){
-            for(int j=i-1;j>=0;j--){
-                if(a[j]==1){
-                    stepf=0.5;
-                    break;
-                }
-                else if(a[j]==-1){
-                    stepf=-1;
-                    break;
-                }
-                else if(a[j]==0 || a[j]==2){
-                    stepf=stepf+0.5;
-                }
-            }
-        }
-        else if(a[i]==0){
-            for(int j=i-1;j>=0;j--){
-                if(a[j]==1){
-                    stepf=1;
-                    break;
-                }
-                else if(a[j]==-1){
-                    stepf=-1;
-                    break;
-                }
-                else if(a[j]==0){
-                    stepf=stepf+1;
-                }
-                else if(a[j]==2){
-                    stepf=stepf+0.5;
-                }
-            }
-        }
-        g[i]=stepf;
+        dist[i] = nearest;
     }
-    for(int i=0;i<=n-2;i++){
-        if(h[i]==-1 && g[i]!=-1)
-        printf("%.1f ",h[i]);
+
+    // Right to Left pass
+    nearest = INF;
+    for (int i = n - 1; i >= 0; i--) {
+        if (arr[i] == 1) {
+            nearest = 0.0;
+        } else if (arr[i] == -1) {
+            nearest = INF;
+        } else if (nearest < INF) {
+            nearest += (arr[i] == 2) ? 0.5 : 1.0;
+        }
+        if (dist[i] > nearest) {
+            dist[i] = nearest;
+        }
     }
+
+    // Printing the results
+    for (int i = 0; i < n; i++) {
+        printf("%.1f ", (dist[i] == INF) ? -1.0 : dist[i]);
+    }
+    printf("\n");
 
     return 0;
 }
-    
